e59f45e44e19b2d1fea9580a6c222b8a90a1d4f7
john-veillette <johnv@uchicago.edu>
2017-02-10 17:08:26 -0600

    Added code to remove teachers that aren't at cs4all schools from dataframe. Still needs to be tested because I had to pull additional school  data from the city data portal to match the department id from the HR dataset to the to the school name from the cs4all rolodex, which I believe to be identical to the short_name values from the data portal but am not 100% sure

--- school_counts.py
+++ school_counts.py
@@ -5,6 +5,8 @@ import pandas as pd
 import sys
 import os
 from cert_exploder import explode, parse_args
+import requests
+import json
 
 
 def main(filename):
@@ -22,6 +24,30 @@ def main(filename):
     Input: filename of Excel file (to avoid raising an error, the file 
             needs the column names 'Accomplishment' and 'Certification')
 	'''
+    endorsements, certifications, teachers = explode(filename)
+    #remove teachers who aren't assigned to schools
+    teachers = teachers[teachers['DeptType'] == 'ES' or teachers['DeptType'] == 'HS']
+    #remove teachers at non-cs4all schools
+    if os.path.isfile('Cohorts 1-3 rolodex.xlsx'):
+        #get school codes for all cs4all schools
+        cs4all_schools = pd.read_excel('Cohorts 1-3 rolodex.xlsx', sheetname = 'master')
+        cs4all_school_names = cs4all_schools["School"].tolist()
+        #Request school data from city data portal
+        url = "https://data.cityofchicago.org/resource/76dk-7ieb.json"
+        resp = requests.get(url)
+        schools = resp.json() #List of dicts representing each cps school
+        name_to_id = {}
+        for school in schools:
+            name_to_id[row['short_name']] = row['school_id']
+        cs4all_school_ids = [name_to_id[name] for name in cs4all_school_names]
+        #remove teachers with school id not in list of cs4all school codes
+        teachers = teacher[teachers['Deptid'] in cs4all_school_ids]
+    schools = list(set(teachers['Department'].tolist()))
+    for school in schools:
+        '''
+        Function unfinished
+        '''
+
 
 
 if __name__ == '__main__':
